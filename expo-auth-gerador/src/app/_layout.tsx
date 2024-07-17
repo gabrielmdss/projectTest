ae8f18534b4829b9e9fe3e52d61c61984289567b
Gabriel <gersongabriel_@hotmail.com>
2024-07-17 18:14:57 -0300

    login com google funcionando, sem redirecionamento para pagina

---
+++ expo-auth-gerador/src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import {Slot} from 'expo-router';
+import {ClerkProvider} from "@clerk/clerk-expo";
+
+const PUBLIC_CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string
+
+export default function Layout(){
+    return (
+        <ClerkProvider publishableKey={PUBLIC_CLERK_PUBLISHABLE_KEY}>
+            <Slot />
+        </ClerkProvider>
+    )
+}
--- expo-auth-gerador/src/app/index.tsx
+++ expo-auth-gerador/src/app/index.tsx
@@ -1,13 +1,56 @@
+import { useEffect, useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native';
-import { router } from 'expo-router'
+import * as WebBrowser from 'expo-web-browser';
 
-import Button from '../components/button'
+import Button from '../components/button';
+
+import { useOAuth } from '@clerk/clerk-expo';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function Login() {
+    const [isLoading, setIsLoading] = useState(false)
+    
+    const googleOAuth = useOAuth({strategy: "oauth_google"})
+
+    async function loginComGoogle (){
+        try {
+            setIsLoading(true)
+
+            const oAuthFlow = await googleOAuth.startOAuthFlow();
+
+            if(oAuthFlow.authSessionResult?.type === 'success'){
+                if(oAuthFlow.setActive){
+                    await oAuthFlow.setActive({ session: oAuthFlow.createdSessionId })
+                }
+            } else {
+                setIsLoading(false)
+            }
+
+        } catch (error) {
+            console.log(error)
+            setIsLoading(false)
+        }
+    }
+
+
+
+    useEffect(()=> {
+        WebBrowser.warmUpAsync()
+
+        return () => {
+            WebBrowser.coolDownAsync()
+        }
+    })
  return (
    <View style={styles.container}>
     <Text style={styles.title}>Entrar</Text>
-    <Button icon="logo-google" title="Entrar com Google" onPress={() => router.navigate("(auth)")}/>
+    <Button 
+    icon="logo-google" 
+    title="Entrar com Google" 
+    onPress={loginComGoogle}
+    isLoading={isLoading}
+    />
    </View>
   );
 }
