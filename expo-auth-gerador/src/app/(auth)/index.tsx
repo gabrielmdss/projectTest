8d29f07b10a1cd500e6951cc80b4943041ca149a
Gabriel <gersongabriel_@hotmail.com>
2024-07-17 20:12:05 -0300

    funcionando voltar exibir e gerar senha

--- expo-auth-gerador/src/app/(auth)/index.tsx
+++ expo-auth-gerador/src/app/(auth)/index.tsx
@@ -1,17 +1,64 @@
+import { useState } from 'react';
 import Button from '@/components/button';
 import { useAuth, useUser } from '@clerk/clerk-expo';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Modal } from 'react-native';
+import  Slider from '@react-native-community/slider';
+import {ModalPassword} from '@/components/modal'
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 export default function Home(){
     const { user } = useUser()
     const { signOut } = useAuth()
     const userName = user?.firstName;
 
+    const [size, setSize] = useState(4)
+    const [passwordValue, setPasswordValue] = useState("");
+    const [modalVisible, setModalVisible] = useState(false);
+
+    function generatePassword (){
+        let password = ''
+       
+    
+        for (let i = 0, n = charset.length; i < size; i++){
+          password += charset.charAt(Math.floor(Math.random() * n))
+        }
+    
+        setPasswordValue(password)
+        setModalVisible(true);
+      }
+
     return (
         <View style={styles.container}>
             <Image source={{uri: user?.imageUrl}} style={styles.image}/>
             <Text style={styles.text}>Olá {userName}</Text>
+
+            <View style={styles.area}>
+                <Slider
+                style={{ height: 30 }}
+                minimumValue={4}
+                maximumValue={8}
+                thumbTintColor='#FFF'     
+                minimumTrackTintColor='#FFF' 
+                value={size}
+                onValueChange={(value) => setSize(value.toFixed(0)) }   
+                />
+            </View>
+
+            <TouchableOpacity style={styles.button} onPress={generatePassword}>
+                <Text style={styles.buttonText}>Gerar senha com {size } caracteres</Text>
+            </TouchableOpacity>
+
+            <Modal visible={modalVisible} animationType='fade' transparent={true}>
+                <ModalPassword password={passwordValue} handleClose={() => setModalVisible(false)}/>
+            </Modal>
+
+
+            <TouchableOpacity style={styles.button}>
             <Button icon="exit" title='Sair' onPress={() => signOut()}/>
+            </TouchableOpacity>
+
+            
         </View>
     )
 }
@@ -32,5 +79,29 @@ const styles = StyleSheet.create({
         width: 92,
         height: 92,
         borderRadius: 12
-    }
+    },
+    button: {
+        backgroundColor: '#000',
+        width: '80%',
+        height: 65,
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        borderRadius: 8,
+        marginBottom: 18,
+        padding: 1
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 20, 
+      },
+    area: {
+        marginTop: 14,
+        marginBottom: 14,
+        width: '80%',
+        backgroundColor: '#000',
+        borderRadius: 8,
+        padding: 1
+    },
+    
 })
---
+++ expo-auth-gerador/src/components/modal/index.tsx
@@ -0,0 +1,70 @@
+import { View, Text, StyleSheet, TouchableOpacity, Pressable } from "react-native";
+
+export function ModalPassword ({password, handleClose}){
+    return (
+        <View style={styles.container}>
+            <View style={styles.content}>
+            <Text style={styles.title}>Senha</Text>
+
+            <Pressable style={styles.innerPassword}>
+                <Text style={styles.text}>
+                    {password}
+                </Text>
+            </Pressable>
+
+            <View style={styles.buttonArea}>
+                <TouchableOpacity style={styles.button} onPress={handleClose}>
+                    <Text style={styles.buttonText}>Voltar</Text>
+                </TouchableOpacity>
+            </View>
+            </View>
+        </View>
+    )
+
+};
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "rgba(24,24,24,0.6)",
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    content: {
+        backgroundColor: "#FFF",
+        width: '82%',
+        paddingTop: 24,
+        paddingBottom: 24,
+        alignItems: "center",
+        justifyContent: "center",
+        borderRadius: 8,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: '#000',
+        marginBottom: 25,
+    },
+    innerPassword: {
+        backgroundColor: "#000",
+        width: '90%',
+        padding: 14,
+        borderRadius: 8,
+        
+
+    },
+    text: {
+        color: '#FFF',
+        textAlign: "center",
+    },
+    buttonArea: {
+        flexDirection: "row",
+    },
+    button: {
+        marginTop: 18,      
+    },
+    buttonText: {
+        fontWeight: "bold",
+        fontSize: 18
+    }
+})
